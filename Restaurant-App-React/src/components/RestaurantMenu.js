649c45fce522719248349f574d4399b48a151fa7
Himanshu Soni <sonihimanshu2210@gmail.com>
2025-03-01 23:55:28 +0530

    Added Restaurant Menu

---
+++ Restaurant-App-React/src/components/RestaurantMenu.js
@@ -0,0 +1,16 @@
+const RestaurantMenu = () => {
+  return (
+    <div>
+      <h1>Menu</h1>
+      <h2>Restaurant</h2>
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+        <li>Item 4</li>
+      </ul>
+    </div>
+  );
+};
+
+export default RestaurantMenu;
